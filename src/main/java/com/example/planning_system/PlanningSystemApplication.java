28da11d847816a7bb4d71b88240ed6ac5d2a7e56
Erika <erikasteba@gmail.com>
2023-08-15 11:18:23 +0300

    Initial commit

---
+++ src/main/java/com/example/planning_system/PlanningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.planning_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/planning_system/PlanningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planning_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
