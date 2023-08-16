2ed973f7b073b7cf316cd098dffda8853c5e2a21
Erika <erikasteba@gmail.com>
2023-08-16 14:22:23 +0300

    test2

--- src/main/java/com/example/planning_system/models/Day.java
+++ src/main/java/com/example/planning_system/models/Day.java
@@ -7,6 +7,7 @@ public class Day {
         this.dayOfMonth = dayOfMonth;
     }
 
+
     public int getDayOfMonth() {
         return dayOfMonth;
     }
--- src/main/java/com/example/planning_system/models/User.java
+++ src/main/java/com/example/planning_system/models/User.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class User {
 
 
+
     @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
