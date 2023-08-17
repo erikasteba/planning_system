809f3a4c13176f7b55e8acbd0d792c9e3e897b49
Erika <erikasteba@gmail.com>
2023-08-17 17:14:10 +0300

    activities get saved in bdb

--- src/main/java/com/example/planning_system/controllers/ActivitiesController.java
+++ src/main/java/com/example/planning_system/controllers/ActivitiesController.java
@@ -1,17 +1,30 @@
 package com.example.planning_system.controllers;
 
 import com.example.planning_system.models.Activities;
+import com.example.planning_system.models.User;
+import com.example.planning_system.repositories.ActivitiesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+
+
 
 @Controller
 public class ActivitiesController {
+
+    @Autowired
+    private ActivitiesRepository activitiesRepository;
+
 
     @GetMapping("/calendar/activities")
     public String showActivityForm() {
@@ -23,31 +36,50 @@ public class ActivitiesController {
             @RequestParam String start_time,
             @RequestParam String end_time,
             @RequestParam(defaultValue = "false") boolean is_public,
-            Model model){
+            Model model) {
 
-        String errorMessage="";
-        try {
-            LocalDateTime startDateTime = LocalDateTime.parse(start_time);
-            LocalDateTime endDateTime = LocalDateTime.parse(end_time);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-            model.addAttribute("activity_name", activity_name);
-            model.addAttribute("is_public", is_public);
+        Long userId = null;
+        LocalDateTime startDateTime = null;
+        LocalDateTime endDateTime = null;
+        if (authentication != null && authentication.getPrincipal() instanceof User) {
+            User user = (User) authentication.getPrincipal();
+            userId = user.getId();
 
-            if (startDateTime.isAfter(endDateTime)) {
-                errorMessage = "Please choose correct dates or time.";
+
+            String errorMessage = "";
+            try {
+                startDateTime = LocalDateTime.parse(start_time);
+                endDateTime = LocalDateTime.parse(end_time);
+
+                //model.addAttribute("activity_name", activity_name);
+                //model.addAttribute("start_time", start_time);
+                //model.addAttribute("end_time", end_time);
+                //model.addAttribute("is_public", is_public);
+
+                if (startDateTime.isAfter(endDateTime)) {
+                    errorMessage = "Please choose correct dates or time.";
+                    model.addAttribute("errorMessage", errorMessage);
+                    return "day-details";
+                }
+            } catch (DateTimeParseException e) {
+                errorMessage = "Invalid date and time format.";
                 model.addAttribute("errorMessage", errorMessage);
                 return "day-details";
             }
-        }catch (DateTimeParseException e) {
-            errorMessage = "Invalid date and time format.";
-            model.addAttribute("errorMessage", errorMessage);
-            return "day-details";
+
+
         }
 
-        errorMessage = "";
+
+        Activities activities = new Activities(activity_name, startDateTime.toString(), endDateTime.toString(), is_public);
+        activitiesRepository.save(activities);
+
+
         return "day-details";
 
-    }
 
+    }
 
 }
--- src/main/java/com/example/planning_system/models/Activities.java
+++ src/main/java/com/example/planning_system/models/Activities.java
@@ -6,24 +6,38 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Data
 @ToString
 @Entity
 @Table(name = "\"activities\"")
 public class Activities {
+
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long user_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+
     private String name;
-    @JsonFormat(pattern = "HH:mm a")
-    private String startTime;
-    private int startMonth;
-    private int startDay;
+
     @JsonFormat(pattern = "HH:mm")
-    private String endTime;
-    private int endMonth;
-    private int endDay;
+    private LocalTime startTime;
+
+    private LocalDate startDate;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime endTime;
+
+    private LocalDate endDate;
+
     private boolean isPublic;
 
     // I am not sure if I've made it correctly :D
@@ -33,15 +47,14 @@ public class Activities {
     public Activities() {
     }
 
-    public Activities(String name, String startTime, int startMonth, int startDay,
-                      String endTime, int endMonth, int endDay, boolean isPublic) {
+    public Activities(String name, String startTime, String endTime, boolean isPublic) {
+        LocalDateTime start = LocalDateTime.parse(startTime);
+        LocalDateTime end = LocalDateTime.parse(endTime);
         this.name = name;
-        this.startTime = startTime;
-        this.startMonth = startMonth;
-        this.startDay = startDay;
-        this.endTime = endTime;
-        this.endMonth = endMonth;
-        this.endDay = endDay;
+        this.startTime = start.toLocalTime();
+        this.startDate = start.toLocalDate();
+        this.endTime = end.toLocalTime();
+        this.endDate = end.toLocalDate();
         this.isPublic = isPublic;
     }
 }
