02e70dc325c4da520629d709de83c98dc9cb30c4
anastasijaOstrovska <anastasija.ostrovska@edu.rtu.lv>
2023-08-17 12:10:23 +0300

    to index

--- src/main/java/com/example/planning_system/configurations/SecurityConfig.java
+++ src/main/java/com/example/planning_system/configurations/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig{
                 .formLogin(form -> form
                 .loginPage("/user/login")
                 .permitAll()
-                .defaultSuccessUrl("/homepage", true)
+                .defaultSuccessUrl("/index", true)
                 )
                 .logout(logout -> logout
                 .permitAll()
