3f54eb1b18b4ce2a879df45f77e53791fb9774a2
Alise Jece <alisejece@gmail.com>
2023-08-17 14:37:30 +0300

    User profile

---
+++ src/main/java/com/example/planning_system/controllers/ProfileController.java
@@ -0,0 +1,51 @@
+package com.example.planning_system.controllers;
+
+import com.example.planning_system.models.User;
+import com.example.planning_system.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/user/profile")
+    public String userProfile(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        String username = userDetails.getUsername();
+        User user = userRepository.findByUsername(username);
+        model.addAttribute("user", user);
+        return "profile";
+    }
+
+    @PostMapping("/user/update-username")
+    public String updateUsername(@AuthenticationPrincipal UserDetails userDetails, String newUsername) {
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        user.setUsername(newUsername);
+        userRepository.save(user);
+        return "redirect:/user/profile";
+    }
+
+    @PostMapping("/user/update-email")
+    public String updateEmail(@AuthenticationPrincipal UserDetails userDetails, String newEmail) {
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        user.setEmail(newEmail);
+        userRepository.save(user);
+        return "redirect:/user/profile";
+    }
+
+    @PostMapping("/user/update-password")
+    public String updatePassword(@AuthenticationPrincipal UserDetails userDetails, String newPassword) {
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        user.setPassword(newPassword);
+        userRepository.save(user);
+        return "redirect:/user/profile";
+    }
+}
+
