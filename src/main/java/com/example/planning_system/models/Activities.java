72051b3b9beb41a29f7da074ab4f56bee527f946
Arthur Rukoly <arthurrukoly000@gmail.com>
2023-08-17 13:00:27 +0300

    callendar-day-buttons

--- src/main/java/com/example/planning_system/models/Activities.java
+++ src/main/java/com/example/planning_system/models/Activities.java
@@ -2,55 +2,46 @@ package com.example.planning_system.models;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
 
-import java.time.LocalTime;
+@Data
+@ToString
 @Entity
 @Table(name = "\"activities\"")
 public class Activities {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long user_id;
-
     private String name;
-
-    public Long getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
+    @JsonFormat(pattern = "HH:mm a")
+    private String startTime;
+    private int startMonth;
+    private int startDay;
     @JsonFormat(pattern = "HH:mm")
-    private LocalTime time;
+    private String endTime;
+    private int endMonth;
+    private int endDay;
+    private boolean isPublic;
 
-
-    public void setId(Long id) {
-        this.id = id;
+    // I am not sure if I've made it correctly :D
+    // @ManyToOne
+    // @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    // private User user;
+    public Activities() {
     }
 
-    public Long getId() {
-        return id;
+    public Activities(String name, String startTime, int startMonth, int startDay,
+                      String endTime, int endMonth, int endDay, boolean isPublic) {
+        this.name = name;
+        this.startTime = startTime;
+        this.startMonth = startMonth;
+        this.startDay = startDay;
+        this.endTime = endTime;
+        this.endMonth = endMonth;
+        this.endDay = endDay;
+        this.isPublic = isPublic;
     }
 }
