2a2a65a47758eec3fa02d74ee4cbe6b1b6920794
Alise Jece <alisejece@gmail.com>
2023-08-21 12:16:13 +0300

    User profile

--- src/main/java/com/example/planning_system/controllers/ProfileController.java
+++ src/main/java/com/example/planning_system/controllers/ProfileController.java
@@ -1,27 +1,21 @@
 package com.example.planning_system.controllers;
 
 import com.example.planning_system.models.User;
-import com.example.planning_system.repositories.UserRepository;
 import com.example.planning_system.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class ProfileController {
-    @Autowired
     private final PasswordEncoder passwordEncoder;
     private final UserService userService;
 
@@ -32,24 +26,27 @@ public class ProfileController {
     }
 
     @PostMapping("/update-username")
-    public String updateUsername(@AuthenticationPrincipal User currentUser, String newUsername) {
+    public String updateUsername(@AuthenticationPrincipal User currentUser, String newUsername, RedirectAttributes redirectAttributes) {
         currentUser.setName(newUsername);
         userService.updateUser(currentUser);
+        redirectAttributes.addFlashAttribute("updateUsernameMessage", "Username updated successfully.");
         return "redirect:/user/profile";
     }
 
     @PostMapping("/update-email")
-    public String updateEmail(@AuthenticationPrincipal User currentUser, String newEmail) {
+    public String updateEmail(@AuthenticationPrincipal User currentUser, String newEmail, RedirectAttributes redirectAttributes) {
         currentUser.setEmail(newEmail);
         userService.updateUser(currentUser);
+        redirectAttributes.addFlashAttribute("updateEmailMessage", "Email updated successfully.");
         return "redirect:/user/profile";
     }
 
     @PostMapping("/update-password")
-    public String updatePassword(@AuthenticationPrincipal User currentUser, String newPassword) {
+    public String updatePassword(@AuthenticationPrincipal User currentUser, String newPassword, RedirectAttributes redirectAttributes) {
         String encodedPassword = passwordEncoder.encode(newPassword);
         currentUser.setPassword(encodedPassword);
         userService.updateUser(currentUser);
+        redirectAttributes.addFlashAttribute("updatePasswordMessage", "Password updated successfully.");
         return "redirect:/user/profile";
     }
 }
