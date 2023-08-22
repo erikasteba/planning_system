2198ba278c4bc94ff022679c377bbec73a475612
anastasijaOstrovska <anastasija.ostrovska@edu.rtu.lv>
2023-08-22 09:05:16 +0300

    front change1

--- src/main/java/com/example/planning_system/controllers/UserController.java
+++ src/main/java/com/example/planning_system/controllers/UserController.java
@@ -1,13 +1,19 @@
 package com.example.planning_system.controllers;
 
-import ch.qos.logback.core.model.Model;
 import com.example.planning_system.models.User;
 import com.example.planning_system.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,21 +28,39 @@ public class UserController {
        return "registration";
     }
 
+    @GetMapping("/updatePassword")
+    public String updatePas(){
+        return "updatePassword";
+    }
+
     @PostMapping("/user/registration")
-    public String createUser(User user, Model model){
-        if (!userService.createUser(user)){
-            model.addText("errorMessage");
+    public String createUser(User user,
+                             @RequestParam String password,
+                             Model model) {
+        String errorMessage = "";
+
+        try {
+
+            if ( (password.length() < 8)||(password.length() > 30) ){
+                errorMessage = "This password must be 8-30 symbols";
+                model.addAttribute("errorMessage", errorMessage);
+                return "registration";
+            }
+        } catch (DateTimeParseException e) {
+            errorMessage = "Invalid date and time format.";
+            model.addAttribute("errorMessage", errorMessage);
+            return "registration";
+        }
+        if (!userService.createUser(user)) {
+            errorMessage = "User with this email already exists";
+            model.addAttribute("errorMessage", errorMessage);
             return "registration";
         }
         return "redirect:/user/login";
+
+
     }
-    @GetMapping("/hello")
-    public String securityUrl(){
-        return "hello";
-    }
-    @GetMapping("/homepage")
-    public String home(){
-        return "homepage";
-    }
+
+
 
 }
